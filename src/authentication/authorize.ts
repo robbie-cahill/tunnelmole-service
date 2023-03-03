be3e4cba26988a3a1fdc2340f094c8e4c4290d5a
Robbie Cahill <robbie-cahill@users.noreply.github.com>
2023-03-03 21:27:00 +1100

    Fix typo

--- src/authentication/authorize.ts
+++ src/authentication/authorize.ts
@@ -5,7 +5,7 @@ export default async function authorize(apiKey: string) : Promise<boolean> {
     const apiKeys = JSON.parse(fs.readFileSync(ROOT_DIR + "/src/authentication/apiKeys.json").toString());
 
     const apiKeyRecord = apiKeys.find((record: any) => {
-        return record.apiKey = apiKey;
+        return record.apiKey == apiKey;
     });
 
     if (typeof apiKeyRecord !== 'undefined') {
