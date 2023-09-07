bc6126cbd4f12be8ec9d910e0b2c5a88df090831
Robbie Cahill <robbie-cahill@proton.me>
2023-09-07 22:33:19 +1000

    Update test

--- src/reserved-domain/reserved-domain.test.ts
+++ src/reserved-domain/reserved-domain.test.ts
@@ -1,14 +1,20 @@
-import { findBySubdomain } from "../repository/reserved-subdomain-repository";
+import { findBySubdomain, findSubdomainsNotBelongingToApiKey } from "../repository/reserved-subdomain-repository";
 import { reserveDomain } from "./reserved-domain";
 
 jest.mock("../repository/reserved-subdomain-repository");
 const mockedFindBySubdomain = findBySubdomain as jest.MockedFunction<typeof findBySubdomain>;
 
+const mockedFindSubdomainsNotBelongingToApiKey = findSubdomainsNotBelongingToApiKey as jest.MockedFunction<typeof findBySubdomain>;
+
 describe("reserveDomain", () => {
-    it("should return true if domain is reserved successfully", async () => {
+    it("should return true if no other users own this subdomain", async () => {
         mockedFindBySubdomain.mockImplementation(async () => {
             return Promise.resolve(undefined);
         });
+
+        mockedFindSubdomainsNotBelongingToApiKey.mockImplementation(async() => {
+            return Promise.resolve(undefined);
+        })
 
         const reservedDomain = { apiKey: "test-api-key", subdomain: "example" };
         const result = await reserveDomain(reservedDomain);
@@ -16,7 +22,7 @@ describe("reserveDomain", () => {
     });
 
     it("should return false if the domain is already reserved by a different apiKey", async () => {
-        mockedFindBySubdomain.mockImplementation(async () => {
+        mockedFindSubdomainsNotBelongingToApiKey.mockImplementation(async () => {
             return Promise.resolve({ 
                 apiKey: "other-api-key", 
                 subdomain: "example" 
