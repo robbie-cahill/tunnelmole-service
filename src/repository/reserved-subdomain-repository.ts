7f314a4e666454adab06c22313fb240fa762ee69
Robbie Cahill <robbie-cahill@proton.me>
2023-09-07 21:28:02 +1000

    Fix SQL

--- src/repository/reserved-subdomain-repository.ts
+++ src/repository/reserved-subdomain-repository.ts
@@ -6,23 +6,27 @@ const RESERVED_DOMAINS = 'reserved_domains'
 
 const findBySubdomain = async (subdomain: string): Promise<ReservedDomain|undefined> => {
     const connection = getConnection();
-    const result = await connection.execute(`
+    const sql = `
         SELECT * FROM ${RESERVED_DOMAINS} 
-        WHERE subdomain = '${subdomain}'
-    `);
-    
+        WHERE subdomain = ${escape(subdomain)}
+    `;
+    const result = await connection.execute(sql);
+
+    console.log(sql);
     return Promise.resolve(result[0]);
 }
 
 const addReservedDomain = async (reservedDomain: ReservedDomain) => {
     const connection = getConnection();
-    await connection.execute(`
+    const sql = `
         INSERT INTO ${RESERVED_DOMAINS} (apiKey, subdomain)
         VALUES (
-            '${escape(reservedDomain.apiKey)}',
-            '${escape(reservedDomain.subdomain)}'
+            ${escape(reservedDomain.apiKey)},
+            ${escape(reservedDomain.subdomain)}
         )
-    `);
+    `;
+    console.log(sql);
+    await connection.execute(sql);
 }
 
 export {
