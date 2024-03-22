d3a1929098e52a4b12795a334e9d9524263af28d
Sabine Schaller <sabine@interledger.org>
2024-03-22 11:04:08 +0100

    only getConnection when logging enabled

--- src/metrics/add-client-log.ts
+++ src/metrics/add-client-log.ts
@@ -3,12 +3,13 @@ import config from "../../config";
 import { getConnection } from "../mysql/connection";
 
 export default async function addClientLog(clientId: string, eventKey: string, eventValue: string): Promise<void> {
-    const connection = await getConnection();
     const enableLogging = config.runtime.enableLogging ?? false;
 
     if (!enableLogging) {
         return;
     }
+
+    const connection = await getConnection();
 
     const date = moment().tz('Australia/Sydney').format('YYYY-MM-DD HH:mm:ss');
     await connection.query(
