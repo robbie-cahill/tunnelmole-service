89589d693745884a7f503227047fbef6c7694e6f
Robbie Cahill <robbie-cahill@proton.me>
2024-06-14 21:27:39 +1000

    Use a MySQL connection pool instead of just one connection

--- src/mysql/connection.ts
+++ src/mysql/connection.ts
@@ -1,22 +1,20 @@
 import config from "../../config";
-
 import mysql from 'mysql2/promise';
 
-let connection: mysql.Connection;
+const pool = mysql.createPool({
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+    waitForConnections: true,
+    connectionLimit: 10, // You can adjust this based on your needs
+    queueLimit: 0
+});
 
 const getConnection = async () => {
-    if (!connection) {
-        connection = await mysql.createConnection({
-            host     : config.mysql.host,
-            user     : config.mysql.user,
-            password : config.mysql.password,
-            database : config.mysql.database
-        });
-    }
-
-    return connection;
+    return pool;
 }
 
 export {
-    getConnection
+    getConnection,
 };
