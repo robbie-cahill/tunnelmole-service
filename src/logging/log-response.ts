c880a74be7cd11299ec918f3d9c1b04d823c9eaa
Robbie Cahill <robbie-cahill@proton.me>
2024-12-14 18:43:29 +1100

    Fix compariosn

--- src/logging/log-response.ts
+++ src/logging/log-response.ts
@@ -1,7 +1,7 @@
 import ForwardedResponseMessage from "../messages/forwarded-response-message"
 
 const logResponse = (response: ForwardedResponseMessage, tunnelmoleSubdomain: string) => {
-    if (process.env.TUNNELMOLE_LOG_LEVEL = "debug") {
+    if (process.env.TUNNELMOLE_LOG_LEVEL == "debug") {
         console.info(`Response from ${tunnelmoleSubdomain}: ${JSON.stringify(response)}`)
     }
 }
