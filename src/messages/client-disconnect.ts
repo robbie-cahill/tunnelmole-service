b0e8f4cdedf97eb24a21aa04306b8bf10209cce4
Robbie Cahill <robbie-cahill@proton.me>
2025-01-25 12:37:49 +1100

    Improve interface

--- src/messages/client-disconnect.ts
+++ src/messages/client-disconnect.ts
@@ -2,6 +2,6 @@
  * Sent to get the client to disconnect/exit with a message to be printed to the console
  */
 export default interface ClientDisconnect {
-    type: string,
+    type: "clientDisconnect",
     exitCode?: number
 }
